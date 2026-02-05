cfae18a2119f076c04d2636976de04f7dba89421
Raincloud <raincloudthedragon@gmail.com>
2026-02-04 23:46:16 -0700

    VersionedCode fix all failing versions

--- src/main/java/me/mgin/graves/mixin/ClientPlayerInteractionManagerMixin.java
+++ src/main/java/me/mgin/graves/mixin/ClientPlayerInteractionManagerMixin.java
@@ -35,6 +35,8 @@ public class ClientPlayerInteractionManagerMixin {
             //? if >1.20.2 {
             target = "Lnet/minecraft/block/Block;onBreak(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;Lnet/minecraft/entity/player/PlayerEntity;)Lnet/minecraft/block/BlockState;"
             //?} else {
+            /*target = "Lnet/minecraft/block/Block;onBreak(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;Lnet/minecraft/entity/player/PlayerEntity;)Lnet/minecraft/block/BlockState;"
+            */
             target = "Lnet/minecraft/block/Block;onBreak(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;Lnet/minecraft/entity/player/PlayerEntity;)V"
             //?}
         ),
